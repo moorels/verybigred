28b09578822ddbc92c031a5649fbc4202297bb7f
moorels <sales@micropls.com>
2021-09-12 14:29:49 +1000

    r4e5ert

--- web/src/layouts/HeadzLayout/HeadzLayout.js
+++ web/src/layouts/HeadzLayout/HeadzLayout.js
@@ -20,8 +20,8 @@ const HeadzLayout = ({ children }) => {
   return (
     <>
       <div className={styles.body}>
-        <header className="text-gray-600 body-font ">
-          <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center ">
+        <header className="text-gray-600 body-font max-w-screen-md">
+          <div className="container  flex flex-wrap p-5 flex-col md:flex-row items-center ">
             <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 ">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
